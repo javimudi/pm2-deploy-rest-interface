878c6433b6b2b251f26048d197c74ccc202b37b2
Javier Muñoz Díaz <javi@MacBook-Pro.local>
2016-06-20 22:24:10 +0200

    Basic server, GET and deployment file reading

---
+++ index.js
@@ -0,0 +1,40 @@
+var pm2 = require('pm2');
+var restify = require('restify');
+var kue = require('kue');
+var JSON5 = require('json5');
+var fs = require('fs');
+var env = require('process').env;
+var kue = require('kue');
+var express = require('express');
+var debug = {
+	info: require('debug')('info'),
+	warn: require('debug')('warn'),
+	error: require('debug')('error')
+}
+var ospath = require('path');
+var port = env.PM2RPORT || 8090;
+var kueport = env.KUE_PORT || 8091;
+var appDir = ospath.join(ospath.dirname(module.filename),'./');
+var deploymentFile = ospath.join(appDir, 'deployment.json5');
+var eco = fs.readFileSync(deploymentFile);
+
+
+// Super simple server
+var server = restify.createServer({ name: 'pm2-rest-interface' });
+
+
+server.get('/(.*)', function(req, res){
+	res.send(JSON5.parse(eco).deploy);
+})
+
+
+server.listen(port, function(){
+	debug.info("Starting pm2-rest-interface on port " + port);
+});
+
+
+var app_kue = express();
+app_kue.use(kue.app);
+app_kue.listen(kueport,  function(){
+     debug.info("Kue UI listening on port " + kueport + "...");
+});
